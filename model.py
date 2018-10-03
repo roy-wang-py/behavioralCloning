7dcef7140e49c7fb6071457347fcbd9fb3c80a52
roy-wang-py <wpyroy@qq.com>
2018-10-03 13:48:12 +0800

    Mod video file name. Add comments in file model.py

--- model.py
+++ model.py
@@ -13,6 +13,8 @@ from scipy import ndimage
 
 
 samples = []
+
+#file dir: file dir where data was stored.
 file_dir = "/opt/carnd_p3/data"
 #file_dir = "/home/workspace/data"
 with open(file_dir+'/driving_log.csv') as csvfile:
@@ -21,7 +23,7 @@ with open(file_dir+'/driving_log.csv') as csvfile:
         samples.append(line)
 samples = samples[1::]
 
-
+#split data
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 
@@ -36,29 +38,32 @@ def generator(samples, batch_size=32):
             angles = []
             for batch_sample in batch_samples:
                 for i in range(3):
-                    #name = './IMG/'+batch_sample[i].split('/')[-1]
-                    #name = file_dir+'/IMG/'+batch_sample[i].split('/')[-1]
+                    #As i run simulator in windows, file dir is split by "\" instead of "/" in dataset
                     if batch_sample[i].find('\\') != -1 :
                         name = file_dir+'/IMG/'+batch_sample[i].split('\\')[-1]
                     else:
                         name = file_dir+'/IMG/'+batch_sample[i].split('/')[-1]                    
                     #center_image = cv2.imread(name)
+                    #simulator used RGB, while cv2.imread is BGR
                     center_image = ndimage.imread(name)
                     
                     center_angle = float(batch_sample[3])
                     
                     if i == 0:
+                        #flip center img
                         images.append(center_image)
                         angles.append(center_angle)
                         images.append(cv2.flip(center_image,1))
                         angles.append(center_angle*-1.0)
                     elif i ==1:
+                        #flip left img
                         center_angle = center_angle+correction
                         images.append(center_image)
                         angles.append(center_angle)
                         images.append(cv2.flip(center_image,1))
                         angles.append(center_angle*-1.0)
                     else:
+                        #flip right img
                         center_angle = center_angle-correction
                         images.append(center_image)
                         angles.append(center_angle)
@@ -74,51 +79,55 @@ def generator(samples, batch_size=32):
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
-#ch, row, col = 3, 80, 320  # Trimmed image format
+#define activate function
 activate_func = 'elu'
 
+#create model
 model = Sequential()
 
 # Preprocess incoming data, centered around zero with small standard deviation 
 model.add(Lambda(lambda x: ((x / 255.0) - 0.5), input_shape=(160,320,3)))
-
+#crop images
 model.add(Cropping2D(cropping=((70,25), (0,0))))
 model.add(Conv2D(24,(5,5),strides=(2,2),activation=activate_func)) 
 #model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Dropout(0.5))
+
 model.add(Conv2D(36,(3,3),strides=(2,2),activation=activate_func))
 #model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Dropout(0.5))
+
 model.add(Conv2D(48,(3,3),strides=(2,2),activation=activate_func))
 #model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Dropout(0.5))
+
 model.add(Conv2D(64,(3,3),activation=activate_func))
 #model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Dropout(0.5))
+
 model.add(Conv2D(64,(3,3),activation=activate_func))
 model.add(Flatten())
 model.add(Dropout(0.5))
+
 model.add(Dense(100))
 model.add(Activation(activate_func))
 #model.add(Dropout(0.5))
 model.add(Dense(50))
 model.add(Activation(activate_func))
 #model.add(Dropout(0.5))
+
 model.add(Dense(10))
 model.add(Activation(activate_func))
 #model.add(Dropout(0.5))
+
 model.add(Dense(1))
 model.summary()
 
+#set default learning rate
 model.compile(optimizer=Adam(0.0001), loss="mse")
 #model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator,  nb_val_samples=len(validation_samples), nb_epoch=3)
 
-"""
-If the above code throw exceptions, try 
-model.fit_generator(train_generator, steps_per_epoch= len(train_samples),
-validation_data=validation_generator, validation_steps=len(validation_samples), epochs=5, verbose = 1)
-"""
 model.save('model.h5')  # creates a HDF5 file 'model.h5'
 
 
